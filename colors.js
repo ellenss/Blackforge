a9faadac10a6327c01307f113a8a4ce113f59730
Ellen Samdal <ellensamdal@hotmail.com>
2025-08-26 20:39:14 +0200

    handle lists in redis

--- colors.js
+++ colors.js
@@ -1,32 +1,32 @@
 export async function getCurrentColor(client) {
   return await client.get("currentColor");
-};
+}
 
 export async function getAllColors(client) {
-    return await client.get("colors");
-};
+  return await client.LRANGE("colors", 0, -1);
+}
 
 export function setColor(client, newColor) {
-    client.set("currentColor", newColor);
+  client.set("currentColor", newColor);
 }
 
 export function addColor(client, colorName, colorAmount) {
-    client.RPUSH("colors", "{ name: colorName, amount: colorAmount * 1000 }");
+  client.RPUSH(
+    "colors",
+    JSON.stringify({ name: colorName, amount: colorAmount * 1000 })
+  );
 }
 
 export function updateColorAmount(client, colorName, addColorAmount) {
-    client.get("colors", (err, colors) => {
-        if (err) {
-            console.error("Error fetching colors:", err);
-            return;
-        }
-        let colorList = JSON.parse(colors) || [];
-        let color = colorList.find(c => c.name === colorName);
-        if (color) {
-            color.amount += addColorAmount * 1000;
-            client.set("colors", JSON.stringify(colorList));
-        } else {
-            console.error("Color not found:", colorName);
-        }
-    });
+  client.LRANGE("colors", 0, -1).then((colors) => {
+    let colorList = JSON.parse(colors) || [];
+    let color = colorList.find((c) => c.name === colorName);
+    if (color) {
+      client.LREM("colors", 0, JSON.stringify(color));
+      color.amount += addColorAmount * 1000;
+      client.RPUSH("colors", JSON.stringify(color));
+    } else {
+      console.error("Color not found:", colorName);
+    }
+  });
 }
